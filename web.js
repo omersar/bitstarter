b1def9129cdbe3cba7105d3d8f5349b51e59cb04
Omer Sr <os@gmail.com>
2013-07-12 15:20:21 +0300

    web.js modified to read inde.html

--- web.js
+++ web.js
@@ -2,8 +2,12 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var fs = require('fs');
+
+var str_buff = fs.readFileSync("index.html");
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(Buffer.toString(str_buff));
 });
 
 var port = process.env.PORT || 5000;
